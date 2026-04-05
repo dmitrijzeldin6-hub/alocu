f1f094b20c319abeafb25759d9cb1fd8630038a7
dmitrijzeldin6-hub <dmitrijzeldin6@icloud.com>
2026-04-05 18:55:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -157,4 +157,6 @@ def confirm_spin():
     return jsonify({"success": True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port
